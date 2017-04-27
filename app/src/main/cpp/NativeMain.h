c4a2af8897c7c8a4949d39c9ed485a61ca5be5ca
atsumi <atsumi@mail.bizen.jp>
2017-04-28 01:21:51 +0900

    modify package name

--- app/src/main/cpp/NativeMain.h
+++ app/src/main/cpp/NativeMain.h
@@ -1,14 +1,16 @@
-#ifndef NATIVEANIMATION_NATIVEMAIN_H
-#define NATIVEANIMATION_NATIVEMAIN_H
+#ifndef NATIVEACTIVITY_SAMPLE_NATIVEMAIN_H
+#define NATIVEACTIVITY_SAMPLE_NATIVEMAIN_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+
 #include "android_native_app_glue.h"
+
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif //NATIVEANIMATION_NATIVEMAIN_H
+#endif //NATIVEACTIVITY_SAMPLE_NATIVEMAIN_H
